c6136e3504b8484e90f651c51ebceb05d0bdcdf7
Mateus <mtauws@gmail.com>
2022-06-22 12:02:30 -0300

    chore: add <Title />

---
+++ src/components/Title/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './styles.css';
+const Title = () => {
+  return (
+    <div className="page-title">
+      <h2>Lorem Ipsum</h2>
+    </div>
+  );
+};
+
+export default Title;
