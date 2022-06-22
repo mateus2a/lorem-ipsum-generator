5cec01e21366150a6cb2ec7aa6bd37745e091f34
Mateus <mtauws@gmail.com>
2022-06-22 12:02:17 -0300

    chore: add <Subtitle />

---
+++ src/components/Subtitle/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './styles.css';
+const Subtitle = () => {
+  return (
+    <div className="page-subtitle">
+      <p>The most perfect lorem ipsum in history.</p>
+    </div>
+  );
+};
+
+export default Subtitle;
