4456c99eeb5898074a7dc1f7afd405bc9a5b089c
Mateus <mtauws@gmail.com>
2022-06-22 12:02:41 -0300

    chore: add <Question />

---
+++ src/components/Question/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './styles.css';
+const Question = () => {
+  return (
+    <div className="page-title">
+      <h2>How many paragraphs do you need?</h2>
+    </div>
+  );
+};
+
+export default Question;
