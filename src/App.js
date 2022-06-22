1a31c6866e9a64f7c974cfc5d4af38113e3be2ac
Mateus <mtauws@gmail.com>
2022-06-22 12:02:58 -0300

    chore: update <App />

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import GenerateText from './components/GenerateText';
+import Title from './components/Title';
+// import GenerateText from './components/GenerateText';
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
-      <GenerateText />
+      <Title />
     </div>
   );
 };
